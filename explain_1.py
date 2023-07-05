43391c414ab572bd9ef2b1d63f756db9cc267b45
clone <maripapanikola@gmail.com>
2023-07-05 21:14:50 +0300

    simple changes

--- explain_1.py
+++ explain_1.py
@@ -1,21 +1,15 @@
-# load the model's weights in order to plot the features with their weights
 import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
-# load the model's weights in order to plot the features with their weights
+
 saved_weights = torch.load('outputs/model_1/NN_weights.pt')
 
 # extract the weights from the saved_weights tensor and save them to a list
 weights = []
-# saved_weights is a dictionary with keys the names of the layers and values the weights
 for key, value in saved_weights.items():
-    # the weights are in the first row of the tensor
-    # only one element tensors can be converted to Python scalars
-    # so we can't write weights.append(value[0].item())
-    # instead we write:
     weights.append(value[0].tolist())
        
 
@@ -35,13 +29,6 @@ axs[2].set_title('Third layer weights')
 plt.tight_layout()
 plt.savefig('outputs/model_1/NN_weights.png')
 plt.show()
-
-# plot their relationship with the features if the features are the:
-features = ["gender", "age", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
-            "f9", "f10"]
-
-# and the layers are the:
-layers = ["first layer", "second layer", "third layer"]
 
 # and the neurons are equal to:
 neurons = [first_layer_weights, second_layer_weights, third_layer_weights]
