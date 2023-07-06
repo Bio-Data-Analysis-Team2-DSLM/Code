113d659b359af615c5ec0daca8536e334cb2cfd9
E1en1 <115937350+E1en1@users.noreply.github.com>
2023-07-07 01:07:06 +0300

    Add files via upload

---
+++ balance_dataset.py
@@ -0,0 +1,14 @@
+def balance_dataset(data):
+    # Count the number of samples in each class
+    class_counts = data['target'].value_counts()
+
+    # Find the class with the fewest samples
+    min_count = class_counts.min()
+
+    # Undersample the majority class
+    balanced_df = data.groupby('target').apply(lambda x: x.sample(n=min_count)).reset_index(drop=True)
+
+    # Drop the extra data from the majority class
+    balanced_df.drop_duplicates(inplace=True)
+
+    return balanced_df
