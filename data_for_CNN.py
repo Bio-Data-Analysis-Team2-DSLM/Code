9e990cc3658ced131e573924fa96765de45099f7
clone <maripapanikola@gmail.com>
2023-07-06 17:22:14 +0300

    action_cnn data

--- data_for_CNN.py
+++ data_for_CNN.py
@@ -1,21 +1,51 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('Data/action.csv')
+
+#print(data.head())
+
 # we will simplify the data by taking the mean of every 30 values of the activity
 # for every patient
 # we will drop the rows that are not divisible by 30
-import pandas as pd
 
-data = pd.read_csv('Data/action.csv')
+data2 = pd.DataFrame()
 
 for i in range(1, 56):
     df = data[data['patient'] == i]
     df = df.reset_index(drop=True)
-    df = df.drop(['patient'], axis=1)
-    df = df.drop(['timestamp'], axis=1)
-    df = df.drop(['target'], axis=1)
-    for j in range(0, len(df), 30):
-        df.loc[j] = df.loc[j:j+30].mean()
+    #print(df)
+    #print(len(df))
+    if len(df) % 30 != 0:
+        df = df.drop(df.index[len(df) - len(df) % 30:])
+    #print(len(df))
+    df = df.groupby(np.arange(len(df))//30).mean(numeric_only=True)
+    df = df.round(3)
     df['patient'] = i
+    #print(df)
+    data2 = pd.concat([data2, df], axis=0)
 
-    data = pd.concat([data, df], axis=0)
+data2 = data2.reset_index(drop=True)
+#print(data2)
 
-data = data.reset_index(drop=True)
-print(data)
+# one day has 1440 minutes and we have one measurement every 30 minutes
+# so we have 48 measurements per day
+# for every patient wi will keep the maximum amount of days that is divisible by 48
+
+data3 = pd.DataFrame()
+
+for i in range(1, 56):
+    df = data2[data2['patient'] == i]
+    df = df.reset_index(drop=True)
+    #print(df)
+    #print(len(df))
+    if len(df) % 48 != 0:
+        df = df.drop(df.index[len(df) - len(df) % 48:])
+    #print(len(df))
+    df['patient'] = i
+    #print(df)
+    data3 = pd.concat([data3, df], axis=0)
+
+data3 = data3.reset_index(drop=True)
+print(data3)
+data3.to_csv('Data/action_cnn.csv', index=False)
