c37d2784bd9d5d162f8731a2ffddb1dd32cb6b44
clone <maripapanikola@gmail.com>
2023-07-06 18:26:05 +0300

    cnn for new data

--- delete_later/copied_1D_CNN.py
+++ delete_later/copied_1D_CNN.py
@@ -4,53 +4,66 @@ import torch.nn as nn
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv('Data/action_cnn.csv')
-
+data = pd.read_csv('Data/data_for_feat_extr.csv')
+# create dataframe for data
 data = pd.DataFrame(data)
-print(data)
-data = data.drop(['patient'], axis=1)
 
-# create a new column which will assign the same number to every 48 rows
-data['patient'] = 0
-for i in range(0, 49392, 48):
-    data['patient'][i:i+48] = i/48 + 1
-print(data)
-
-# find the fist patient with target 1
-for i in range(0, 49392, 48):
-    if data['target'][i] == 1:
-        break
-print()
-print(f'first patient with target 1 is patient #{data["patient"][i]} at row {i}')
-print()
-
+# create a list of lists with the features
 features = []
-# create a list of lists with the features of every patient
-# number of patients = 1029  and number of features = 48
-for i in range(1, 1030):
-    df = data[data['patient'] == i]
-    df = df.reset_index(drop=True)
-    df = df.drop(['patient', 'target'], axis=1)
-    df = df.values
-    features.append(df)
+for i in range(0, 55):
+    features.append(data['activity'][i])
+
+# convert string "['1', '2', '3', ...]" to list [1, 2, 3, ...]
+for i in range(0, 55):
+    features[i] = features[i].replace("[", "")
+    features[i] = features[i].replace("]", "")
+    features[i] = features[i].replace("'", "")
+    features[i] = features[i].split(", ")
+    features[i] = list(map(int, features[i]))
+
+# simplify features by taking the mean of every 30 values in a list
+# smooothing the data 
+
+for i in range(0, 55):
+    features[i] = [sum(features[i][j:j+30])/30 for j in range(0, len(features[i]), 30)]
+
+
+# the lists are not of the same length
+# so we need to pad them with the mean of the list
+
+# we need to find the maximum length of the lists
+max_len = 0
+for i in range(0, 55):
+    if len(features[i]) > max_len:
+        max_len = len(features[i])
+
+# pad the lists with the mean of the list for the rest of the values
+for i in range(0, 55):
+    if len(features[i]) < max_len:
+        # if the value is nan for the rest of the values then replace it with the mean of the list
+        # just for the values that are nan, not for the whole list
+        for isnan in range(0, max_len-len(features[i])):
+            mean = sum(features[i])/len(features[i])
+            features[i].append(mean)
+
 
 # normalize the data to have mean 0 and std 1 for every list of features 
-for i in range(0, 1029):
+for i in range(0, 55):
     scaler = StandardScaler()
     features[i] = scaler.fit_transform(np.array(features[i]).reshape(-1, 1))
 # make the lists of the same length
-for i in range(0, 1029):
-    features[i] = features[i].reshape(-1, 1)
+for i in range(0, 55):
+    features[i] = features[i].reshape(546)
 
 # convert the lists to tensors
-for i in range(0, 1029):
+for i in range(0, 55):
     features[i] = torch.tensor(features[i], dtype=torch.float32)
 
 # create the X tensors
 X = torch.stack(features)
 
 # reshape the tensors to have shape (55, 1, 546)
-X = X.view(1029, 1, 48)
+X = X.view(55, 1, 546)
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
@@ -66,10 +79,10 @@ class CNN(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv1d(in_channels=1, out_channels=1, kernel_size=1),
             nn.ReLU(),
-            nn.Linear(48, 10))
+            nn.Linear(546, 10))
         
         self.decoder = nn.Sequential(
-            nn.Linear(10, 48),
+            nn.Linear(10, 546),
             nn.ReLU(),
             nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=1))
 
@@ -115,7 +128,7 @@ for i in range(epochs):
         for g in optimizer.param_groups:
             g['lr'] = g['lr'] * 0.9
 
-    # print every 1000 epochs
+    # print every 20000 epochs
     if i % 1000 == 0:
         print(f'epoch: {i:2} loss: {loss.item():10.3f}')
         # save the loss values as a list [epochs, loss] in order to plot them
@@ -133,7 +146,7 @@ plt.savefig('Data/loss_values_CNN.jpg')
 
 # save the extracted features as a new dataframe
 features = model.encoder(X).detach().numpy()
-features = features.reshape(-1, 10)
+features = features.reshape(-1, 20)
 # create a dataframe with headers f1, f2, ..., f10
 headers = ['f' + str(i) for i in range(1, 11)]
 # add the headers to the dataframe
